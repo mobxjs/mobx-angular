7d2aca2ab7765a9d8fd3195624ff3789178dac0f
Adam Klein <adam@500tech.com>
2017-01-21 12:28:57 +0200

    Updated example

--- example/todos/src/app/stores/todos.ts
+++ example/todos/src/app/stores/todos.ts
@@ -8,24 +8,50 @@ class Todo {
   @action toggle() {
     this.completed = !this.completed;
   }
+  @action complete() {
+    this.completed = true;
+  }
 }
 const todo1 = new Todo('Buy Milk');
 const todo2 = new Todo('Write ng2-mobx connector');
 
 class Todos {
   @observable todos = [todo1, todo2];
-  @observable filter = '';
+  @observable filter = null;
 
   @action addTodo(title) {
     this.todos.push(new Todo(title));
   }
 
+  @action removeTodo(todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  @action showAll() {this.filter = null}
+  @action showCompleted() {this.filter = true}
+  @action showActive() {this.filter = false}
+
+  @action clearCompleted() {
+    this.todos = this._filter(this.todos, false);
+  }
+
+  @action completeAll() {
+    this.todos.forEach((todo) => todo.complete())
+  }
+
   @computed get filteredTodos() {
-    return this.todos.filter((todo) => new RegExp(this.filter, 'i').test(todo.title));
+    return this.filter !== null ?
+      this._filter(this.todos, this.filter) :
+      this.todos;
   }
 
   @computed get uncompletedItems() {
-    return this.todos.filter((todo) => !todo.completed).length;
+    return this._filter(this.todos, false).length;
+  }
+
+  private _filter(todos, value) {
+    return todos.filter((todo) => todo.completed === value);
   }
 }
 export default new Todos();
