ef010466776a2038c4c5ded09453ca3a8cf8308f
Adam Klein <adam@500tech.com>
2017-06-23 23:57:51 +0300

    fixes + add localstorage demo

--- example/todos/src/app/stores/todos.ts
+++ example/todos/src/app/stores/todos.ts
@@ -1,58 +1,74 @@
+import { observable, computed, action, autorun, toJS } from 'mobx';
 
 class Todo {
-  completed = false;
-  title: string;
+  @observable completed = false;
+  @observable title: string;
 
   constructor({ title, completed }) {
     this.completed = completed;
     this.title = title;
   }
 
-  toggle() {
+  @action toggle() {
     this.completed = !this.completed;
   }
-  complete() {
+  @action complete() {
     this.completed = true;
   }
 }
 
 class Todos {
-  todos = [];
-  filter = 'SHOW_ALL';
+  @observable todos = [];
+  @observable filter = 'SHOW_ALL';
 
-  addTodo({ title, completed = false }) {
+  constructor() {
+    this.localStorageSync();
+  }
+
+  @action addTodo({ title, completed = false }) {
     this.todos.push(new Todo({ title, completed }));
   }
 
-  removeTodo(todo) {
+  @action removeTodo(todo) {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
   }
 
-  showAll() { this.filter = 'SHOW_ALL'; }
-  showCompleted() { this.filter = 'COMPLETED'; }
-  showActive() { this.filter = 'ACTIVE'; }
+  @action showAll() { this.filter = 'SHOW_ALL'; }
+  @action showCompleted() { this.filter = 'COMPLETED'; }
+  @action showActive() { this.filter = 'ACTIVE'; }
 
-  clearCompleted() {
+  @action clearCompleted() {
     this.todos = this._filter(this.todos, 'ACTIVE');
   }
 
-  completeAll() {
-    this.todos.forEach((todo) => todo.complete());
+  @action completeAll() {
+    this.todos.forEach((todo) => todo.complete())
   }
 
-  get filteredTodos() {
+  @computed get filteredTodos() {
     return this.filter !== 'SHOW_ALL' ?
       this._filter(this.todos, this.filter) :
       this.todos;
   }
 
-  get uncompletedItems() {
+  @computed get uncompletedItems() {
     return this._filter(this.todos, false).length;
   }
 
   private _filter(todos, value) {
-    return todos.filter((todo) => value === 'ACTIVE' ? !todo.completed : todo.completed);
+    return todos.filter((todo) => value === 'COMPLETED' ? todo.completed : !todo.completed);
+  }
+
+  private localStorageSync() {
+    const initialTodos = JSON.parse(localStorage.todos || '[]');
+    this.todos = initialTodos.map((todo) => new Todo(todo));
+    this.filter = JSON.parse(localStorage.filter || '"SHOW_ALL"');
+
+    autorun(() => {
+      localStorage.todos = JSON.stringify(toJS(this.todos));
+      localStorage.filter = JSON.stringify(toJS(this.filter));
+    });
   }
 }
 
