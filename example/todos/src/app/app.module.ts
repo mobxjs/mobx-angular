ab5d79e346ac657293f0b356d3687a34b280c6fe
Serkan Sipahi <serkan.sipahi@ligatus.com>
2018-02-03 20:09:39 +0100

    reset accidentally committed code!

--- example/todos/src/app/app.module.ts
+++ example/todos/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import remotedev from 'mobx-remotedev/lib/dev';
+import remotedev from 'mobx-remotedev/lib/dev';
 
 import { MobxAngularModule } from 'mobx-angular';
 import { Todos } from './stores/todos.store';
@@ -24,8 +24,7 @@ import { CountComponent } from './components/count/count.component';
     HttpModule,
     MobxAngularModule
   ],
-  // providers: [{ provide: Todos, useClass: remotedev(Todos) }],
-  providers: [{ provide: Todos, useClass: Todos }],
+  providers: [{ provide: Todos, useClass: remotedev(Todos) }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
