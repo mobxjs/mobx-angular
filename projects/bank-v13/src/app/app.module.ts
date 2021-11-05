021185466f1e49248878e1513f2a34850a78133f
Simon <simondataonly@gmail.com>
2021-11-05 17:49:23 +0200

    updated account.store.ts & mobx-reaction.directive.ts

--- projects/bank-v13/src/app/app.module.ts
+++ projects/bank-v13/src/app/app.module.ts
@@ -2,10 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ActionsComponent } from './actions/actions.component';
+import { BalanceComponent } from './balance/balance.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MobxAngularModule } from 'projects/mobx-angular/src/public-api';
+// import { MobxAngularModule } from 'mobx-angular';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ActionsComponent,
+    BalanceComponent,
+    TransactionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    MobxAngularModule,
+    FormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
