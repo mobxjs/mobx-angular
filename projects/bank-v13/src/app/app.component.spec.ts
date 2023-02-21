9c8e654dfb81d3704f319aeae3569efc7c966095
sagarchikane <sagargchikane2014@gmail.com>
2023-02-21 15:49:48 +0000

    Fix unit test warning

--- projects/bank-v13/src/app/app.component.spec.ts
+++ projects/bank-v13/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
